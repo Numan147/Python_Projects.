ce5e6eac090d50533e554b541c2f1b0766d92242
Numan147 <numan2330@gmail.com>
2022-08-22 12:19:25 +0530

    Edit

--- sizefile.py
+++ sizefile.py
@@ -6,13 +6,13 @@ def size_convert(sz): #Convert bytes to KB, or MB or GB
             return "%3.1f %s" % (sz, i)
         sz /= 1024.0
 
-def get_file_size(filepath): #
+def get_file_size(filepath): 
     size = os.path.getsize(filepath)
     print(f'The {filepath} size is', size, 'bytes')
     convert_size = size_convert(size)
     #print('actual file size is ', convert_size) // Programer Can print Directly without assigning a variable.
     return convert_size
 
-file_size = get_file_size(r'C:\Users\admin\Desktop\NiravsTesting\mSociety Documentation v2 (1).pdf')
+file_size = get_file_size(r'C:\Users\admin\Desktop\NiravsTesting\mSociety Documentation v2 (1).pdf') #assigning a variable and printing file size.
 print('Actual file size is ', file_size)
 
